f2c11933d4dd4f288720c4ea627ab0af297f6561
Pawel Gabryelewicz <pawelgabryelewicz@users.noreply.github.com>
2019-07-21 15:47:04 +0200

    Initial version

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import { declare } from "@babel/helper-plugin-utils";
+
+import pluginProposalObjectRestSpread from "@babel/plugin-proposal-object-rest-spread";
+import pluginSyntaxDynamicImport from "@babel/plugin-syntax-dynamic-import";
+import pluginProposalDecorators from "@babel/plugin-proposal-decorators";
+import pluginProposalClassProperties from "@babel/plugin-proposal-class-properties";
+import presetEnv from "@babel/preset-env";
+import presetReact from "@babel/preset-react";
+import presetTypeScript from "@babel/preset-typescript";
+import presetTypeScriptVue from "babel-preset-typescript-vue";
+
+export default declare(
+(api, {
+    vue = false,
+    react = true,
+    presetEnvSettings = {
+      "modules": "auto",
+      "useBuiltIns": "usage",
+      "corejs": 3
+    }
+  }) => {
+  api.assertVersion(7);
+
+  if (typeof vue !== "boolean") {
+    throw new Error("vue must be a boolean, or undefined");
+  }
+
+  if (typeof presetEnvSettings !== "object") {
+    throw new Error("presetEnvSettings must be an object, or undefined");
+  }
+
+  const presets = [];
+
+  presets.push(vue ? presetTypeScriptVue : presetTypeScript);
+
+  presets.push([
+    presetEnv,
+    presetEnvSettings
+  ]);
+
+  if(react) {
+    presets.push(presetReact);
+  }
+
+  const cfg = {
+    "plugins": [
+      pluginProposalObjectRestSpread,
+      pluginSyntaxDynamicImport,
+      [ pluginProposalDecorators, { "legacy": true } ],
+      [ pluginProposalClassProperties, { "loose": true } ]
+    ],
+    presets
+  };
+
+  return cfg;
+
+});
